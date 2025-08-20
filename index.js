fe6499a14ec0cbb9d01441767487dcbaa1fb7d46
Rogelio <roger.hrg@gmail.com>
2025-08-19 19:39:33 -0600

    Update .gitignore and set up Express server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+import express from 'express' //ESM Ecmascript Modules
+
+const app = express()
+
+//Routing
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(4000,() => {
+    console.log('Servidor Funcionando...')
+})
