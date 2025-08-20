ccb5112bfa5b7cef80c6ea9905c830aa7dce4ca0
Rogelio <roger.hrg@gmail.com>
2025-08-19 20:20:46 -0600

    Typescript Instalation

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+//Routing
+app.get('/', (req, res) => {
+    res.send('Hola Mundo con TypeScript y Express')
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port,() => {
+    console.log('Servidor Funcionando en el puerto', port)
+})
