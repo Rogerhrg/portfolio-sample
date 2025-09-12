5bd42e2ab75b55d4af0947af21ac5b09f6047bb7
Rogelio <roger.hrg@gmail.com>
2025-09-12 11:31:37 -0600

    front social links

--- Front-End/src/views/WikiTreeView.tsx
+++ Front-End/src/views/WikiTreeView.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react"
+import { social } from "../data/social" 
+import WikiTreeInput from "../components/WikiTreeInput"
+
 export default function WikiTreeView() {
+  const [wikiTreeLinks, setWikiTreeLinks] = useState(social)
+
   return (
-    <div>WikiTreeView</div>
+    <div className="space-y-5">
+      {wikiTreeLinks.map( item => (
+        <WikiTreeInput
+          key={item.name}
+          item={item}
+        />
+      ))}
+    </div>
   )
 }
